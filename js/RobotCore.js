9117a883382a1bee5659164be8e5b678a72ee109
Bjorn Johansen <bjorn.johansen@xamarin.com>
2016-08-16 22:36:40 +0700

    Add basic setup

---
+++ js/RobotCore.js
@@ -0,0 +1,7 @@
+'use strict';
+
+export default class {
+  run() {
+    console.log('running...');
+  }
+}
---
+++ js/main.js
@@ -0,0 +1,3 @@
+import RobotCore from 'RobotCore.js';
+let core = new RobotCore();
+core.run();
